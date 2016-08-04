66c9c5c0eeab575c3061c0399cf77680392ff842
jorten <475365254@qq.com>
2016-08-04 18:37:41 +0800

    a

--- app.js
+++ app.js
@@ -263,6 +263,6 @@ io.on('connection', (socket) => {
     })
 })
 
-http.listen(3000, function() {
-    console.log("Server listening on port 3000");
+http.listen(80, function() {
+    console.log("Server listening on port 80");
 });
