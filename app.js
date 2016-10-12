6cda16c82771b444df2962c4d52b72aa6b82a454
5606595 <475365254@qq.com>
2016-10-12 23:00:16 +0800

    a

--- app.js
+++ app.js
@@ -880,6 +880,6 @@ function sendMS(num, phoneNum) {
     }
 }
 
-http.listen(3000, function() {
-    console.log("Server listening on port 3000");
+http.listen(80, function() {
+    console.log("Server listening on port 80");
 });
