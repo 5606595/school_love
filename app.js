439701384d2fb5abea6926877a1e73f8c3c94fae
5606595 <475365254@qq.com>
2016-10-13 22:32:07 +0800

    a

--- app.js
+++ app.js
@@ -384,7 +384,7 @@ app.post('/token', urlencodedParser, (req, res) => {
                             }
                         }
                         var xml = builder.buildObject(msg);
-                        var querySel = "update user set gender = 1, weichatNum = '" + result.FromUserName[0] + "', allow = 1 where randomCode = '" + randomCode + "'";
+                        var querySel = "insert into user (gender, weichatNum, phoneNum, password, Name, valiPhoto, contact) values (1, '" + result.FromUserName[0] + "', '1', '223', 'a', '1', '11')";
                         connection.query(querySel, (err, res2) => {
                             if(err) {
                                 console.log(err);
@@ -404,7 +404,7 @@ app.post('/token', urlencodedParser, (req, res) => {
                             }
                         }
                         var xml = builder.buildObject(msg);
-                        var querySel = "update user set gender = 0, weichatNum = '" + result.FromUserName[0] + "', allow = 1 where randomCode = '" + randomCode + "'";
+                        var querySel = "insert into user (gender, weichatNum, phoneNum, password, Name, valiPhoto, contact) values (0, '" + result.FromUserName[0] + "', '1', '223', 'a', '1', '11')";
                         connection.query(querySel, (err, res2) => {
                             if(err) {
                                 console.log(err);
